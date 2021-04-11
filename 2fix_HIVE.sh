0dd69515e55fa8ea01dce3632ba8052420fae8fa
PapakWaqa <PapakWaqa@gmail.com>
2021-04-11 14:41:54 +0800

    first commit

--- 2fix_HIVE.sh
+++ 2fix_HIVE.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+#FAILED: Execution Error, return code -101 from org.apache.hadoop.hive.ql.exec.mr.MapRedTask. DEFAULT_MR_AM_ADMIN_USER_ENV
+#此错误是tez 中hadoop包与hadoop 版本包不一致导致
 sudo rm /opt/apache-tez-0.9.2-bin/lib/hadoop-mapreduce-c*
 sudo cp /opt/hadoop-2.10.1/share/hadoop/mapreduce/hadoop-mapreduce-client-co* /opt/apache-tez-0.9.2-bin/lib/
