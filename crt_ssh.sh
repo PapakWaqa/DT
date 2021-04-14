4e6ca43fb0ef4bfdfa3ce8176cdccbe608a4b734
Linux User <bigred@gw.localdomain>
2021-04-14 10:49:12 +0800

    add ssh_crt

---
+++ crt_ssh.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+hstname="`hostname`" && [ ! $hstname == "gw" ] && echo "must run on gw" && exit 1
+echo $hstname
+which ssh > /dev/null 2>&1
+[ ! "$?" == "0" ] && sudo apk add openssh >/dev/null && echo -e "installed openssh\n"
+[ ! -f ~/.ssh/id_rsa.pub ] && ssh-keygen -q -f $HOME/.ssh/id_rsa -t rsa -P '' 
+echo $USER>>password.txt
+for i in wka01 wka02 wka03 mas01 ds01
+do
+    ssh-keyscan -f ~/.ssh/id_rsa.pub $i >> ~/.ssh/known_hosts 2>/dev/null
+    sshpass -f password.txt ssh-copy-id bigred@$i
+done    
