d3f0e13f951482d68e5e96f39e31799c952aa5d0
PapakWaqa <PapakWaqa@gmail.com>
2021-04-20 15:33:14 +0800

    fix file name

--- 0set_gw.sh
+++ 0set_gw.sh
@@ -5,8 +5,18 @@ sudo apk update && sudo  apk upgrade && sudo apk add sudo nano bash procps pytho
 echo "Copied hosts file to /etc/hosts"
 sudo cp web/config/hosts /etc/hosts
 
+#
+echo -n "Copied mactohost file to /etc/"
+sudo cp web/config/mactohost /etc/mactohost && echo "...OK" || echo "failed"
+
+
+
+echo "Copied busybox_httpd to /bin"
+sudo cp ~/DT/web/pkg/busybox_httpd /bin/busybox_httpd
+
 
 cat ~/.bash_profile 2>/dev/null|grep "DT_HOME" >/dev/null
-[ "$?" == "0" ] && echo "You have been added EVN args. before" && exit 1
+[ "$?" == "0" ] && echo "You have been added EVN args before this time" && exit 1
+
 
 sudo echo -e "export DT_HOME=~/DT\n"'export PATH=$DT_HOME/bin:$PATH'>> ~/.bash_profile && echo "it will reboot after 5 seconds" && sleep 5 && sudo reboot
