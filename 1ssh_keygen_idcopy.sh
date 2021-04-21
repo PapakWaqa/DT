3b86eb8da0ef4f2f57a5d5e7bf72b33e712028b0
PapakWaqa <PapakWaqa@gmail.com>
2021-04-21 20:22:56 +0800

    add userlist

--- 1ssh_keygen_idcopy.sh
+++ 1ssh_keygen_idcopy.sh
@@ -5,7 +5,6 @@ echo $hstname
 which ssh > /dev/null 2>&1
 [ ! "$?" == "0" ] && sudo apk add openssh >/dev/null && echo -e "installed openssh\n"
 [ ! -f ~/.ssh/id_rsa.pub ] && ssh-keygen -q -f $HOME/.ssh/id_rsa -t rsa -P '' 
-echo $USER>>password.txt
 for i in wka01 wka02 wka03 mas01 ds01
 do
     ssh-keyscan -t rsa  $i >> ~/.ssh/known_hosts 2>/dev/null
